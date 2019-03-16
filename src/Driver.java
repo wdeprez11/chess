8aa5548a2d797ff942b496b5b9aaed44b3aae277
William <william@deprez.com>
2019-03-16 13:34:05 -0500

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI gui = new GUI();
+        frame.add(gui);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+
+    }
+}
---
+++ src/GUI.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+public class GUI extends JPanel {
+    public void paint(Graphics g) {
+        super.paint(g);
+        this.setBackground(Color.WHITE);
+
+        g.fillRect(0, 0, 40, 40);
+
+
+    }
+}
